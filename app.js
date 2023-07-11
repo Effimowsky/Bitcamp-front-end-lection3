fb595b0fe098b51d6cc1ecbf1f29d8031117343d
Effimowsky <94548214+Effimowsky@users.noreply.github.com>
2023-07-11 23:55:30 +0400

    update app.js

---
+++ app.js
@@ -0,0 +1,32 @@
+// 19.დაწერეთ პროგრამა რომელიც კონსოლში გამოპრინტავს რიცხვებს 0 დან 10 ის ჩათვლით.
+// Print the numbers from 1 to 10 in the console.
+
+// for (let i = 0; i <= 10; i++) {
+//   console.log(i);
+// }
+
+// 20.დაწერეთ პროგრამა, რათა შეამოწმოთ რიცხვი მარტივია თუ არა.
+// Write a program to check if a number is prime or not.
+
+// function isPrime(num) {
+//   if (num <= 1) {
+//     return false;
+//   }
+//   for (let i = 2; i <= Math.sqrt(num); i++) {
+//     if (num % i === 0) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+
+// console.log(isPrime(2));
+
+// 21.დაწერეთ პროგრამა სტრიქონის შებრუნებისთვის.
+// Write a program to reverse a string.
+
+function string_reverse(str) {
+  return str.split("").reverse().join("");
+}
+
+console.log(string_reverse("Hello"));
